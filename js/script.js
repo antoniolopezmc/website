03fc92b7da9965fa0368517086defcaf8ff5b91a
Antonio López Martínez-Carrasco <anlopezmc@gmail.com>
2020-02-15 18:35:33 +0100

    CVs updated and phone number changed (version 2.3.0 tagged).

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ var hostname_c = 's';
 var phone_number_a = '+34 ';
 var phone_number_b = '868 ';
 var phone_number_c = '88 ';
-var phone_number_d = '7345';
+var phone_number_d = '1853';
 var total = '<big>Contact:</big> ' + phone_number_a + phone_number_b + phone_number_c + phone_number_d + ' · <a href="ma' + 'il' + 'to:' + username_a + username_b + username_c + '@' + hostname_a + hostname_b + hostname_c + '">' + username_a + username_b + username_c + '@' + hostname_a + hostname_b + hostname_c + '</a>';
 document.getElementById("div_for_contact_information").innerHTML = total;
